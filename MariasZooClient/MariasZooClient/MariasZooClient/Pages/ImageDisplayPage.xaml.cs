2eb4899fdaf16ade7184fc4dfea1832d5f18c71f
Wurstbert <Wursti@FLEISCHEREI>
2017-09-09 17:54:11 +0200

    Added working image fetching from server

--- MariasZooClient/MariasZooClient/MariasZooClient/Pages/ImageDisplayPage.xaml.cs
+++ MariasZooClient/MariasZooClient/MariasZooClient/Pages/ImageDisplayPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using MariasZooClient.Models;
+﻿using MariasZooClient.BusinessObjects;
+using MariasZooClient.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.ObjectModel;
 using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -9,14 +13,6 @@ namespace MariasZooClient.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ImageDisplayPage : ContentPage
     {
-        /*ObservableCollection<ImageContainer> imageContainerList = new ObservableCollection<ImageContainer>()
-        {
-            new ImageContainer() { Title = "Katze 1", Date = DateTime.Now, SourceUrl = "http://31.16.100.81:25184/images/cat1.jpg" },
-            new ImageContainer() { Title = "Katze 2", Date = DateTime.Now, SourceUrl = "http://31.16.100.81:25184/images/cat2.jpg" },
-            new ImageContainer() { Title = "Katze 3", Date = DateTime.Now, SourceUrl = "http://31.16.100.81:25184/images/cat3.jpg" },
-            new ImageContainer() { Title = "Katze 4", Date = DateTime.Now, SourceUrl = "http://31.16.100.81:25184/images/cat4.gif" }
-        };*/
-
         public ImageDisplayPage()
         {
             InitializeComponent();
@@ -24,12 +20,18 @@ namespace MariasZooClient.Pages
 
         protected async override void OnAppearing()
         {
-            var result = await ServerModel.Instance.GetNewerImagesThan(new DateTime(2016, 11, 6));
-            Stream receiveStream = await result.Content.ReadAsStreamAsync();
-            StreamReader readStream = new StreamReader(receiveStream);
-            var test = readStream.ReadToEnd();
-            int a = 2;
-            //this.ImageListView.ItemsSource =
+            try
+            {
+                var result = await ServerModel.Instance.GetNewerImagesThan(new DateTime(2016, 11, 6));
+                Stream receiveStream = await result.Content.ReadAsStreamAsync();
+                StreamReader readStream = new StreamReader(receiveStream);
+                var resultString = readStream.ReadToEnd();
+                this.ImageListView.ItemsSource = JsonConvert.DeserializeObject<ObservableCollection<ImageContainer>>(resultString);
+            }
+            catch (Exception e)
+            {
+                // TODO: Show error message in GUI
+            }
         }
     }
 }
